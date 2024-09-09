8a53504ee4fb1c524c49e46336fee89f4e194461
Julien-bl <julien.bulin@epitech.digital>
2024-09-09 17:01:42 +0200

    Create ex_03.js

---
+++ ex_03.js
@@ -0,0 +1,10 @@
+const array [5, 8, 10]
+
+function map(elements, callback) {
+    let = array [5, 8, 10]
+function isEven ( number ) {
+    return number % 2 === 0;
+    }
+    console .log (map ([5 , 8, 10] , isEven ));
+
+    var nouveauTableau = arr.map(callback [, thisArg])// utliser array.map
