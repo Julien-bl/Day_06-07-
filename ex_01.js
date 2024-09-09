2ef19f89563dee897fecf3c125ea8ef5ee43eea3
Julien-bl <julien.bulin@epitech.digital>
2024-09-09 17:00:32 +0200

    Update ex_01.js

--- ex_01.js
+++ ex_01.js
@@ -11,12 +11,3 @@ function resetOpacity () {
 const element = document.getElementById("square");
 element.addEventListener("mouseover", reduceOpacity ())
 element.addEventListener("mouseout", resetOpacity ())
-
-
-//element.addEventListener('mouseover', function(event) {
-   // event.target.style.opacity = "0.5";
- // }, false);
-  
- // element.addEventListener('mouseout', function(event) {
-  // event.target.style.opacity = "1";
-  //}, false);
